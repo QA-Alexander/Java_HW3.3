4b47054f9db23df3a131d4e382adb45e3d9f0297
Alexander Ryzhakov <aryzhakov87@gmail.com>
2024-11-06 23:04:55 +1000

    Initial commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,8 @@
+public class CreditPaymentService {
+    public int calculate(int sum, int month, double percent){
+        double monthPercent = percent / 1200;
+        double annuity = (monthPercent * Math.pow(1 + monthPercent, month))/(Math.pow(1 + monthPercent, month) - 1);
+        double result = sum * annuity;
+        return (int) result;
+    }
+}
