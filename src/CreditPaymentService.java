b92412942c5e93b326adec1dc8771c5b2acda72d
Alexander Ryzhakov <aryzhakov87@gmail.com>
2024-11-12 13:23:04 +1000

    Rework1

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
-    public int calculate(int sum, int month, double percent){
+    public int calculate(int sum, int month, double percent) {
         double monthPercent = percent / 1200;
-        double annuity = (monthPercent * Math.pow(1 + monthPercent, month))/(Math.pow(1 + monthPercent, month) - 1);
+        double annuity = (monthPercent * Math.pow(1 + monthPercent, month)) / (Math.pow(1 + monthPercent, month) - 1);
         double result = sum * annuity;
         return (int) result;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int oneYear = service.calculate(1000000,12,9.99);
-        int twoYears = service.calculate(1000000,24,9.99);
-        int threeYears = service.calculate(1000000,36,9.99);
+        int oneYear = service.calculate(1000000, 12, 9.99);
+        int twoYears = service.calculate(1000000, 24, 9.99);
+        int threeYears = service.calculate(1000000, 36, 9.99);
         System.out.println("Ежемесечная сумма платежа при сроке кредитования 1 год:" + oneYear);
         System.out.println("Ежемесечная сумма платежа при сроке кредитования 2 года:" + twoYears);
         System.out.println("Ежемесечная сумма платежа при сроке кредитования 3 года:" + threeYears);
